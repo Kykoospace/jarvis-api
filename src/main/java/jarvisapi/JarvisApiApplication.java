a9cc7e399bb6a0d211aa3ffb96d39e598ca1316d
Kyllian <kyllian.gt@hotmail.fr>
2020-07-22 22:27:38 +0200

    Spring Boot initialisation

---
+++ src/main/java/jarvisapi/JarvisApiApplication.java
@@ -0,0 +1,13 @@
+package jarvisapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JarvisApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JarvisApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/jarvisapi/JarvisApiApplicationTests.java
@@ -0,0 +1,13 @@
+package jarvisapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JarvisApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
