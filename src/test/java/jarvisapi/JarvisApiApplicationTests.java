3ff0b5c51aad545323c750d38b4dcfa15f7bf33a
Kyllian <kyllian.gt@hotmail.fr>
2020-11-29 00:25:40 +0100

    Init testcontainers, application.properties for test and auto-generation of schema.sql

--- src/test/java/jarvisapi/JarvisApiApplicationTests.java
+++ src/test/java/jarvisapi/JarvisApiApplicationTests.java
@@ -1,13 +1,23 @@
 package jarvisapi;
 
+import org.junit.ClassRule;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.testcontainers.containers.MySQLContainer;
+import org.testcontainers.junit.jupiter.Testcontainers;
 
+@Testcontainers
 @SpringBootTest
 class JarvisApiApplicationTests {
 
+	@ClassRule
+	private static final MySQLContainer mySQLContainer = new MySQLContainer("mysql:8.0.22")
+			.withDatabaseName("jarvis_db_test")
+			.withUsername("jarvis")
+			.withPassword("changeit");
+
 	@Test
 	void contextLoads() {
+		System.out.println("Context loaded");
 	}
-
 }
